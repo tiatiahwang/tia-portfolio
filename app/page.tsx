1688515ea988560079a4b6276017c5ad0f5c330b
tiatiahwang <tiahwang44@gmail.com>
2023-09-07 19:14:27 -0700

    skills

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from '@/components/about';
 import Intro from '@/components/intro';
 import Projects from '@/components/projects';
 import SectionDivider from '@/components/section-divider';
+import Skills from '@/components/skills';
 
 export default function Home() {
   return (
@@ -9,7 +10,10 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <SectionDivider />
       <Projects />
+      <SectionDivider />
+      <Skills />
     </main>
   );
 }
