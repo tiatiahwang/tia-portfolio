d81aa96068158d1cfdfe3a1c1195871c505bc6f5
tiatiahwang <tiahwang44@gmail.com>
2023-10-27 13:35:10 -0700

    add: CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,7 +96,7 @@ const Intro = () => {
           className='group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full 
                     outline-none focus:scale-110 hover:scale-110 active:scale-105 transition 
                     borderBlack dark:bg-white/10'
-          href='/CV.pdf'
+          href='/Tia_Hwang.pdf'
           download
         >
           Download CV{' '}
